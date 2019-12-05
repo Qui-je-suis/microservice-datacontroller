94879cbd635d7d1b64fb6efceb921586bb63d297
cedcass <cassignac.cedric@orange.fr>
2019-12-05 19:40:20 +0100

    first commit

---
+++ src/main/java/mgb/mdatacontroller/MdatacontrollerApplication.java
@@ -0,0 +1,11 @@
+package mgb.mdatacontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdatacontrollerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MdatacontrollerApplication.class, args);
+    }
+}
