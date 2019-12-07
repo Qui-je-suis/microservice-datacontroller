519a3057f18d39278fe56ac63ce59eff72260c2b
cedcass <cassignac.cedric@orange.fr>
2019-12-07 20:26:27 +0100

    test restriction rules

--- src/main/java/mgb/mdatacontroller/MdatacontrollerApplication.java
+++ src/main/java/mgb/mdatacontroller/MdatacontrollerApplication.java
@@ -1,7 +1,7 @@
 /**
- *-------------------------------------------------------------------------
+ * -------------------------------------------------------------------------
  * Copyright (C) 2019 QUI JE SUIS !!!. All rights reserved
- *-------------------------------------------------------------------------
+ * -------------------------------------------------------------------------
  */
 package mgb.mdatacontroller;
 
@@ -9,8 +9,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MdatacontrollerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(MdatacontrollerApplication.class, args);
-    }
+public class MdatacontrollerApplication
+{
+  public static void main(String[] args)
+  {
+    SpringApplication.run(MdatacontrollerApplication.class, args);
+  }
 }
